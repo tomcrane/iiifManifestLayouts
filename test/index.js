be3f15e8e1f3b11e4eed6c5a89b7f80386475a37
Tom Crane <tom@nanoplanet.com>
2016-05-22 23:21:15 +0100

    gh-pages

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import assert from 'assert';
-import iiifManifestD3OsdLayout from '../lib';
-
-describe('iiif-manifest-d3-osd-layout', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
-});
